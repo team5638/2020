875cbc7fa9e6d7fbf5375056ecbad7fea99844a2
team5638 <draddatz@lqpv.org>
2019-10-23 19:45:57 -0500

    comments

--- TestBot/src/main/java/frc/robot/OI.java
+++ TestBot/src/main/java/frc/robot/OI.java
@@ -16,13 +16,11 @@ public class OI {
   // One type of button is a joystick button which is any button on a
   //// joystick.
   // You create one by telling it which joystick it's on and which button
-  // number it is.
   // Joystick stick = new Joystick(port);
   // Button button = new JoystickButton(stick, buttonNumber);
-
+  // Dave WAS HERE
   // There are a few additional built in buttons you can use. Additionally,
   // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
 
   //// TRIGGERING COMMANDS WITH BUTTONS
   // Once you have a button, it's trivial to bind it to a button in one of
